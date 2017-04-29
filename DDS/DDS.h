6195e06a769cccfa6d8a0b281c6355049b66a9e8
A.Vilezhaninov <a.vilezhaninov@gmail.com>
2017-04-30 01:09:48 +0300

    add DDS generator

---
+++ DDS/DDS.h
@@ -0,0 +1,10 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef DDS_H
+#define DDS_H
+
+
+/* Exported functions prototypes ---------------------------------------------*/
+int DDS_GetSinVal(void);
+
+
+#endif /* DDS_H */
